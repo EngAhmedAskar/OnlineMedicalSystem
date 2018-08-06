31cd812bb11fe9f1cc878c58660e92ddf5ab0e13
EngAhmedAskar <eng.ahmedaskar@yahoo.com>
2018-08-05 22:02:43 -0500

    .

--- src/main/java/com/OMS/ClinicProject/model/Speciality.java
+++ src/main/java/com/OMS/ClinicProject/model/Speciality.java
@@ -22,7 +22,52 @@ public class Speciality {
     @NotEmpty(message = "*Please provide description")
     private String description;
 
+    public Long getId() {
+        return id;
+    }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public List<Clinic> getClinicList() {
+        return ClinicList;
+    }
+
+    public void setClinicList(List<Clinic> clinicList) {
+        ClinicList = clinicList;
+    }
+
+    public List<MedicalProfessional> getMedicalProfessionals() {
+        return medicalProfessionals;
+    }
+
+    public void setMedicalProfessionals(List<MedicalProfessional> medicalProfessionals) {
+        this.medicalProfessionals = medicalProfessionals;
+    }
+
+    public Speciality(@NotEmpty(message = "*Please provide name") String name, @NotEmpty(message = "*Please provide description") String description, List<Clinic> clinicList, List<MedicalProfessional> medicalProfessionals) {
+        this.name = name;
+        this.description = description;
+        ClinicList = clinicList;
+        this.medicalProfessionals = medicalProfessionals;
+    }
 
     @ManyToMany(mappedBy = "SpecialityList")
     private List<Clinic> ClinicList;
